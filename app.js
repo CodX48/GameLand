7f9057d5e6bf647fa3a678c481599965e904f6da
Khatab <codx207@gmail.com>
2024-08-25 17:17:22 +0300

    every thing so far

---
+++ app.js
@@ -0,0 +1,14 @@
+var div = document.getElementById('hiden-bar');
+
+
+function hideShow(){
+    if(div.style.display == 'none'){
+        div.style.display = 'block';
+       
+    }
+    else
+    {
+        div.style.display = 'none';
+        
+    }
+}
